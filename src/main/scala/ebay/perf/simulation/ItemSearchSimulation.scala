baf1849a111edf5a443dd0d5c0e708d01d7eea82
Joseph Hughes <josephhughes14@me.com>
2017-05-16 09:47:21 +0100

    Test working with 1 item

--- src/main/scala/ebay/perf/simulation/ItemSearchSimulation.scala
+++ src/main/scala/ebay/perf/simulation/ItemSearchSimulation.scala
@@ -1,12 +1,31 @@
-package ebay.perf
+package ebay.perf.simulation
 
+import java.util.concurrent.TimeUnit.SECONDS
+
+import ebay.perf.Properties._
+import ebay.perf.Assertions._
+import ebay.perf.simulation.ItemSearchSimulation._
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
+import scala.concurrent.duration.FiniteDuration
+
 package object ItemSearchSimulation {
 
+  def searchForItem = exec(http("Search for an item")
+      .get("http://www.ebay.co.uk/sch/i.html?&_nkw=iphone")
+      .check(status.is(200))
+  )
 }
 
 class ItemSearchSimulation extends Simulation {
 
+  val searchForItemScenario = scenario("Item Search")
+    .repeat(repeatTimes) {
+      exec(searchForItem)
+    }
+
+  setUp(
+    searchForItemScenario.inject(rampUsers(userCount) over FiniteDuration(5, SECONDS))
+  ).assertions(assertList)
 }
