5bf2da1df894ba44c838136bb721c7ddeaeda37f
Joseph Hughes <josephhughes14@me.com>
2017-05-16 09:25:04 +0100

    Basic compilation now working, had to roll back scala and gradle versions

---
+++ src/main/scala/ebay/perf/simulation/ItemSearchSimulation.scala
@@ -0,0 +1,12 @@
+package ebay.perf
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+package object ItemSearchSimulation {
+
+}
+
+class ItemSearchSimulation extends Simulation {
+
+}
